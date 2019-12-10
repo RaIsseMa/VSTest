7ab76aa97a90b0ab27f739cfec12571a7bee683f
abdu <bluemanbusiness@gmail.com>
2019-12-10 12:36:42 +0100

    test upload net

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Etudiant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Etudiant
+    {
+        private int cin;
+        private int cne;
+        private int codeG;
+        private string nom;
+
+
+    }
+}
